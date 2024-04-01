05e266f0fa2e0adeb5b2068f1aa3462c17193844
dashel ruiz <dashel.ruiz@protonmail.com>
2024-04-01 17:42:13 -0600

    Adding terraform files

---
+++ terrafile/variables.tf
@@ -0,0 +1,32 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/oregon-crime-e130b790c75d.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  default = "oregon-crime"
+}
+
+variable "region" {
+  description = "Region"
+  default = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default = "US"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default = "oregon-crime-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default = "STANDARDs"
+}
